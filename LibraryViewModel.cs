3192416d14a6ab22699235f292471322f364be19
Adın Soyadın <mmusayeva@std.beu.edu.az>
2026-02-22 14:02:42 +0400

    First commit - all project files

---
+++ LibraryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication3.Models
+{
+    public class LibraryViewModel
+    {
+        public Book Book { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication3.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int ExperienceYears { get; set; }
+    }
+}
